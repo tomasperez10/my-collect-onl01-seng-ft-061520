34ff6a5567648795b1f0a1fcd2519cee2eae1a29
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 22:56:21 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(array)
+  
+  counter = 0
+  
+  collection = []
+  
+  while counter < array.length do
+    collection.push yield(array[counter])
+    counter += 1
+  end
+  collection
+end
 
