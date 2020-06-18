6961fb49488236825b69a8fe99acebc91dce50d7
Tomas Perez <tomasitopg6@gmail.com>
2020-06-18 22:53:06 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,6 @@
 def my_collect(array)
   
-  counter = 0
-  collection = []
   
-  if array == []
-    my_collect(array) do | x |
-      counter += 1
-    end
-  elsif array == collection
-    my_collect(collection) do | lang |
-      lang.upcase
-    end
-  else my_collect(array) do | name |
-    name.split(" ").first
-  end
-end
   
 end
 
